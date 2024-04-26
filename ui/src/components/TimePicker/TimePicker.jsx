bd87e2c230ce10dd016b59aefb54c551fdc141c9
loukanos12 <160498134+loukanos12@users.noreply.github.com>
2024-04-26 13:04:55 +0200

    iets

--- ui/src/components/TimePicker/TimePicker.jsx
+++ ui/src/components/TimePicker/TimePicker.jsx
@@ -4,14 +4,18 @@ import './TimePicker.scss';
 import { t } from 'i18next';
 
 class TimePicker extends React.PureComponent {
-  maxDate = new Date( new Date())
+  maxDate = new Date(new Date());
+
   render() {
-    return <DateTimePickerComponent placeholder ={t('timepicker.placeholder')} 
-    id="datetimepicker" 
-    strictMode={true} 
-    max={this.maxDate} 
-    onChange={(date) =>console.log(date)}
-    />;
+    return (
+      <DateTimePickerComponent
+        placeholder={t('timepicker.placeholder')}
+        id="datetimepicker"
+        strictMode="true"
+        max={this.maxDate}
+        onChange={(date) => console.log(date)}
+      />
+    );
   }
 }
 export default TimePicker;
